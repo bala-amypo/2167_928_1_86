b016d517f568989bc612cfeaf8892e9e11ca4033
bala-amypo <bala@amypo.in>
2025-12-23 05:02:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/ Fertilizer.java
+++ src/main/java/com/example/demo/entity/ Fertilizer.java
@@ -1,7 +1,14 @@
 package com.example.demo.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fertilizers")
 public class Fertilizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String npkRatio;
     private String recommendedForCrops;
@@ -15,13 +22,15 @@ public class Fertilizer {
         this.recommendedForCrops = recommendedForCrops;
     }
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
     public String getNpkRatio() { return npkRatio; }
     public void setNpkRatio(String npkRatio) { this.npkRatio = npkRatio; }
+
     public String getRecommendedForCrops() { return recommendedForCrops; }
     public void setRecommendedForCrops(String recommendedForCrops) { this.recommendedForCrops = recommendedForCrops; }
 }
