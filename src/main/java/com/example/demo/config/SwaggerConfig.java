a2c83a981642a9123180ad7404dacb1574cbf319
bala-amypo <bala@amypo.in>
2025-12-28 12:20:51 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,13 +2,26 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI api() {
-        return new OpenAPI().info(new Info().title("Crop & Fertilizer API").version("1.0"));
+
+        Server productionServer = new Server();
+        productionServer.setUrl("https://9181.408procr.amypo.ai");
+        productionServer.setDescription("Production Server");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Crop & Fertilizer API")
+                        .version("1.0"))
+                .servers(List.of(productionServer));
     }
 }
